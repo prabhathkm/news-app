4d4cb5b2bef1dd291ddc18d49b43ae2799926365
prabhathkm <prabhathkm@gmail.com>
2021-12-21 15:29:32 +0530

    date formatting added to client

--- client/src/components/NewsItem.jsx
+++ client/src/components/NewsItem.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 
 const NewsItem = ({content}) => {
 
+    const formatDate = (dateStr) => {
+        if(!dateStr) return "N/A";
+
+        var date = new Date(dateStr);
+        var dd = String(date.getDate()).padStart(2, '0');
+        var mm = String(date.getMonth() + 1).padStart(2, '0');
+        var yyyy = String(date.getFullYear());
+
+        return `${dd}/${mm}/${yyyy}`;
+    }
+
     return (
         <div className="news-item">
             <h3 className="title">{content.webTitle}</h3>
-            <div className="date">{content.webPublicationDate}</div>
+            <div className="date">{formatDate(content.webPublicationDate)}</div>
             <a href={content.webUrl} target="_blank">View ⧉</a>
         </div>
     );
